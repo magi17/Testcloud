ff94d8aec2580bd1d9af7d2f50c3e5d990f7b976
magi17 <yen-evade-subsidy@duck.com>
2025-02-10 12:07:53 +0000

    Ts

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const { withNextOnPages } = require("@cloudflare/next-on-pages");
 
-module.exports = nextConfig
+module.exports = withNextOnPages({
+  reactStrictMode: true,
+  experimental: {
+    appDir: true, // If using Next.js 13+ with App Router
+  },
+});
